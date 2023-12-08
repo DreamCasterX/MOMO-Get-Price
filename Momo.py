9abe6688ccebbc8cb5d68182b80fb83fa25d5368
Mike Lu <mcumaple@hotmail.com>
2023-12-08 17:30:06 +0800

    test render

--- Momo.py
+++ Momo.py
@@ -8,12 +8,11 @@ import os
 
 url = "https://www.momoshop.com.tw/goods/GoodsDetail.jsp?i_code=8299663&Area=search&mdiv=403&oid=1_2&cid=index&kw=%E7%99%BD%E9%B4%BF2000"
 url2 = "https://www.momoshop.com.tw/goods/GoodsDetail.jsp?i_code=10776280&Area=search&mdiv=403&oid=1_4&cid=index&kw=%E6%BE%8E%E6%BE%8E%20850"
-url3 = "https://www.momoshop.com.tw/goods/GoodsDetail.jsp?i_code=7419315&Area=search&mdiv=403&oid=1_1&cid=index&kw=%E6%BE%8E%E6%BE%8Esoft"
-url4 = "https://www.momoshop.com.tw/goods/GoodsDetail.jsp?i_code=10127162&Area=search&mdiv=403&oid=1_1&cid=index&kw=%E7%99%BD%E9%B4%BF2000"
-url5 = "https://www.momoshop.com.tw/goods/GoodsDetail.jsp?i_code=7040002&Area=search&mdiv=403&oid=1_3&cid=index&kw=%E7%99%BD%E9%B4%BF2000"
-url6 = "https://www.momoshop.com.tw/goods/GoodsDetail.jsp?i_code=8935705&Area=search&mdiv=403&oid=1_1&cid=index&kw=%E6%B3%A1%E8%88%922800"
+url3 = "https://www.momoshop.com.tw/goods/GoodsDetail.jsp?i_code=10127162&Area=search&mdiv=403&oid=1_1&cid=index&kw=%E7%99%BD%E9%B4%BF2000"
+url4 = "https://www.momoshop.com.tw/goods/GoodsDetail.jsp?i_code=7040002&Area=search&mdiv=403&oid=1_3&cid=index&kw=%E7%99%BD%E9%B4%BF2000"
+url5 = "https://www.momoshop.com.tw/goods/GoodsDetail.jsp?i_code=8935705&Area=search&mdiv=403&oid=1_1&cid=index&kw=%E6%B3%A1%E8%88%922800"
 
-products = [url, url2, url3, url4, url5, url6]
+products = [url, url2, url3, url4, url5]
 
 now = datetime.now()  # 取得當前時間
 nowformat = now.strftime("%Y/%m/%d %H:%M:%S")  # 自定義時間格式
@@ -33,7 +32,7 @@ for item in products:
     price = root.find("ul", class_="prdPrice")
     print("目前售價: " + price.span.text + "元\n")   # 顯示當前折扣價格
 
-print(input(""))
+# print(input(""))
 
 
 #   if title.a != None:  # 如果標題包含a標籤 (沒有被刪除), 印出來
